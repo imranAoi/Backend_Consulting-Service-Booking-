8a8fab0ca48acd38b9ae406e7b663af913db0565
imranAoi <ansarimran987@gmail.com>
2026-07-07 22:38:37 +0530

    trim google key

--- config/google.js
+++ config/google.js
@@ -1,11 +1,21 @@
+
 import { google } from "googleapis";
 import dotenv from "dotenv";
 dotenv.config();
 
+function getPrivateKey() {
+  let key = process.env.GOOGLE_PRIVATE_KEY || "";
+  
+  key = key.trim().replace(/^["']|["']$/g, "");
+  
+  key = key.replace(/\\n/g, "\n");
+  return key;
+}
+
 const auth = new google.auth.JWT(
   process.env.GOOGLE_CLIENT_EMAIL,
   null,
-  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  getPrivateKey(),
   ["https://www.googleapis.com/auth/calendar"]
 );
 
